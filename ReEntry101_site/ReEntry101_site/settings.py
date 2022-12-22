83b17310d6fc84bbd29cd4e005b696a407683a66
Christy131 <christyhayter@Christys-MBP.lan>
2022-12-21 19:31:19 -0500

    urls

--- ReEntry101_site/ReEntry101_site/settings.py
+++ ReEntry101_site/ReEntry101_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reentry_todo.apps.ReentryTodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
