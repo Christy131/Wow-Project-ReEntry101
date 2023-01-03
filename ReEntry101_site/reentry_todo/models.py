306f6fba555ef32b85dfe57c35c13c10bdc63925
Mikayla Munn <mikayla.munn@gmail.com>
2023-01-02 20:39:01 -0500

    Created forms and models for question

--- ReEntry101_site/reentry_todo/models.py
+++ ReEntry101_site/reentry_todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    description = models.CharField(max_length = 255)
+    
+class Comment(models.Model):
+    body = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add = True)
--- ReEntry101_site/reentry_todo/urls.py
+++ ReEntry101_site/reentry_todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from reentry_todo.views import HomeView
+from reentry_todo.views import HomeView, CommentDetailView, QuestionsView
 
 urlpatterns=[
     path ('', HomeView.as_view(), name='home'),
-
+    path('questions', QuestionsView.as_view(), name ='question'),
+    
 ]
