fa9587e64234832ea8ea879b42a0230d2934ba74
Mikayla Munn <mikayla.munn@gmail.com>
2023-01-09 20:13:58 -0500

    Changed site url

--- ReEntry101_site/ReEntry101_site/urls.py
+++ ReEntry101_site/ReEntry101_site/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reentry/', include('reentry_todo.urls')),
+    path('', include('reentry_todo.urls')),
 ]
